5ba81735a246cd9e72fa35daae31d13f18b4656e
ksenikeev <ksenikeev@mail.ru>
2024-12-18 13:02:01 +0300

    list2direction

--- lab12_interface/src/main/java/ru/itis/inf403/lab12/list2/Apple.java
+++ lab12_interface/src/main/java/ru/itis/inf403/lab12/list2/Apple.java
@@ -2,10 +2,24 @@ package ru.itis.inf403.lab12.list2;
 
 public class Apple implements CanCompare {
 
+    private String color;
+    private int weight;
+    private String kind;
+    private int price;
 
+    public Apple(String color, int weight, String kind, int price) {
+        this.color = color;
+        this.weight = weight;
+        this.kind = kind;
+        this.price = price;
+    }
 
     @Override
     public int compare(CanCompare other) {
-        return 0;
+        return this.price - ((Apple)other).price;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
--- lab12_interface/src/main/java/ru/itis/inf403/lab12/list2/Main.java
+++ lab12_interface/src/main/java/ru/itis/inf403/lab12/list2/Main.java
@@ -3,5 +3,21 @@ package ru.itis.inf403.lab12.list2;
 public class Main {
     public static void main(String[] args) {
 
+        List2Direction list2 = new List2Direction();
+
+        list2.add(new Apple("Красный", 55, "Антоновка", 120));
+        list2.add(new Apple("Зеленый", 60, "Грени", 100));
+        list2.add(new Apple("Желтый", 80, "Голден", 130));
+        list2.add(new Apple("Алый", 75, "Гала", 150));
+        list2.add(new Apple("Сливовый", 70, "Спартан", 90));
+        list2.add(new Apple("Красно-белый", 65, "Башкирская красавица", 110));
+
+        list2.print();
+
+        list2.add(3, new Apple("Зеленый", 64,
+                "Семеринская", 125));
+
+        list2.print();
+
     }
 }
