9568b37e36ab28bfa4681f94de37280242637474
ksenikeev <ksenikeev@mail.ru>
2025-03-22 13:31:42 +0300

    Fruit

--- lab2_02/src/main/java/ru/itis/inf403/lab2_02/set/Fruit.java
+++ lab2_02/src/main/java/ru/itis/inf403/lab2_02/set/Fruit.java
@@ -21,4 +21,28 @@ public class Fruit {
     public String toString() {
         return "Fruit: " + name + ", " + sugar + "%";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        //if (!o.getClass().equals(this.getClass())) return false;
+        if (this == o) return true;
+        Fruit v = (Fruit) o;
+
+        if (this.name.equals(v.name) && this.sugar == v.sugar)
+            return true;
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int h = super.hashCode();
+        for (int i = 0; i < name.length(); ++i) {
+            h = h ^ name.charAt(i);
+        }
+
+        h = h * sugar * 17;
+        return h;
+    }
 }
--- lab2_02/src/main/java/ru/itis/inf403/lab2_02/set/Main.java
+++ lab2_02/src/main/java/ru/itis/inf403/lab2_02/set/Main.java
@@ -2,6 +2,7 @@ package ru.itis.inf403.lab2_02.set;
 
 public class Main {
     public static void main(String[] args) {
+/*
         Set403Impl<Integer> set = new Set403Impl<>();
 
         set.add(1);
@@ -11,9 +12,15 @@ public class Main {
         set.add(2);
 
         System.out.println(set);
+*/
+        Set403Impl<Fruit> set = new Set403Impl<>();
+        set.add(new Fruit("Яблоко", 5));
+        set.add(new Fruit("Груша", 7));
+        set.add(new Fruit("Апельсин", 2));
+        set.add(new Fruit("Яблоко", 5));
 
-        Integer a[] = set.getAll(new Integer[0]);
-        for(Integer i : a) {
+        Fruit a[] = set.getAll(new Fruit[0]);
+        for(Fruit i : a) {
             System.out.println(i);
         }
 
