25cbbce80c9179d7271fd14f1d88f06317feaf23
ksenikeev <ksenikeev@mail.ru>
2025-05-17 13:24:26 +0300

    db

--- lab2_12_db/src/main/java/ru/itis/inf403/lab2_12/Main.java
+++ lab2_12_db/src/main/java/ru/itis/inf403/lab2_12/Main.java
@@ -6,12 +6,16 @@ import ru.itis.inf403.lab2_12.model.Student;
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setName("Artem");
-        student.setLastName("Kuzmin");
-        student.setFatherName("Ruslanovich");
+        student.setName("Семенов");
+        student.setLastName("Юлиан");
+        student.setFatherName("Цезаревич");
         student.setGroup("11-403");
-        student.setId(14);
+        student.setId(45);
 
-        DBUtils.appendObject(student);
+        DBUtils.readAll().forEach(s -> {
+            System.out.println(s.getId() + " " + s.getName());
+        });
+
+        //DBUtils.appendObject(student);
     }
 }
--- lab2_12_db/src/main/java/ru/itis/inf403/lab2_12/db/DBUtils.java
+++ lab2_12_db/src/main/java/ru/itis/inf403/lab2_12/db/DBUtils.java
@@ -3,6 +3,8 @@ package ru.itis.inf403.lab2_12.db;
 import ru.itis.inf403.lab2_12.model.Student;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBUtils {
     public static final String TABLE = "db/student.tbl";
@@ -32,5 +34,38 @@ public class DBUtils {
         } catch (IOException e) {
             throw new RuntimeException("Не удалось записать данные");
         }
+    }
+
+    public static List<Student> readAll() {
+        List<Student> result = new ArrayList<>();
+        File file = new File(TABLE);
+        if (!file.exists()) {
+            return null;
+        }
+        try (DataInputStream dis = new DataInputStream(
+                new FileInputStream(file))) {
+            while (true) {
+                int id = dis.readInt();
+                byte flag = dis.readByte();
+                int size = dis.readInt();
+                byte[] data = new byte[size];
+                dis.read(data);
+                if (flag == 1) {
+                    try (ObjectInputStream ois = new ObjectInputStream(
+                            new ByteArrayInputStream(data)
+                    )) {
+                        Student student = (Student) ois.readObject();
+                        result.add(student);
+                    }
+                }
+            }
+        } catch (EOFException e) {}
+        catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return result;
+
     }
 }
