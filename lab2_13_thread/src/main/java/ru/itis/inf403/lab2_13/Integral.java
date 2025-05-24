29e2694b20d25aa5f0151ddbb226420930561e87
ksenikeev <ksenikeev@mail.ru>
2025-05-24 13:33:28 +0300

    thread

--- lab2_13_thread/src/main/java/ru/itis/inf403/lab2_13/Integral.java
+++ lab2_13_thread/src/main/java/ru/itis/inf403/lab2_13/Integral.java
@@ -1,9 +1,35 @@
 package ru.itis.inf403.lab2_13;
 
-public class Integral {
+import java.util.function.Consumer;
+
+public class Integral implements Consumer<Double>{
 
     private static int N;
-    private static double integral = 0;
+    private volatile double integral = 0;
+
+    public synchronized void accept(Double d) {
+        integral += d;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // [1, 3]
+        Integral integralObject = new Integral();
+        int countProc = Runtime.getRuntime().availableProcessors();
+        N = 10000 / countProc; // количество "столбиков" для суммирования задачей
+        double h = (3. - 1.) / countProc; // ширина интервала для обсчета каждым потоком
+        Thread[] threads = new Thread[countProc];
+        for (int i = 0; i < countProc; ++i) {
+            threads[i] =
+                    new Thread(new PartSummCalculate(1 + i * h, 1 + (i + 1) * h, integralObject));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < countProc; ++i) {
+            threads[i].join();
+        }
+
+        System.out.println(integralObject.integral);
+    }
 
     public static double func(double x) {
         return Math.exp(- x * x /2) * Math.sin(x);
@@ -22,12 +48,13 @@ public class Integral {
     static class PartSummCalculate implements Runnable {
         private double a;
         private double b;
-        public PartSummCalculate(double a, double b) {
-            this.a = a; this.b = b;
+        private Consumer<Double> consumer;
+        public PartSummCalculate(double a, double b, Consumer<Double> consumer) {
+            this.a = a; this.b = b; this.consumer = consumer;
         }
         public void run() {
             double result = partSum(a, b, N);
-            integral += result;
+            consumer.accept(result);
         }
     }
 }
