63c64452cf1abc40f708beece4edcb8b58998793
ksenikeev <ksenikeev@mail.ru>
2025-06-02 13:58:05 +0300

    thread

--- lab2_14_musik_player/src/main/java/ru/itis/inf403/lab2_14/Main.java
+++ lab2_14_musik_player/src/main/java/ru/itis/inf403/lab2_14/Main.java
@@ -1,7 +1,9 @@
 package ru.itis.inf403.lab2_14;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Playlist playlist = new Playlist();
         playlist.load();
         playlist.add(new Track( "Black eyes", "Chaikovskiy", ""));
